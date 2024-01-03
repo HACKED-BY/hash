db494965a208f9782f08654656a33ada3de3245c
HACKED-BY <140115424+HACKED-BY@users.noreply.github.com>
2024-01-03 07:53:18 +0330

    Add files via upload

---
+++ hash.py
@@ -0,0 +1,52 @@
+from time import sleep
+from colorama import Fore
+from os import system
+import hashlib
+sleep(1)
+
+system("python2 -m pip install colorama")
+
+print """
+hash cracker version 1.0
+
+coded by M R - D A R K
+
+Telegram: https://t.me/Firabil45
+
+[*]-md5
+[*]-sha1
+[*]-sha224
+[*]-sha256
+[*]-sha384
+[*]-sha512
+"""
+try:
+	type_hash = raw_input("Enter type hash: ")
+	hashing = raw_input("Enter Your hash: ")
+	file = raw_input("Enter wordlist: ")
+	print ""
+	wordlist = open(file,"r").readlines()
+
+	for password in wordlist:
+		password = password.strip()
+
+		if type_hash == "md5":
+			hash_a = hashlib.md5(password).hexdigest()
+		elif type_hash == "sha1":
+			hash_a = hashlib.sha1(password).hexdigest()
+		elif type_hash == "sha224":
+			hash_a = hashlib.sha224(password).hexdigest()
+		elif type_hash == "sha256":
+			hash_a = hashlib.sha256(password).hexdigest()
+		elif type_hash == "sha384":
+			hash_a = hashlib.sha384(password).hexdigest()
+		elif type_hash == "sha512":
+			hash_a = hashlib.sha512(password).hexdigest()
+		if hashing == hash_a:
+			print Fore.LIGHTWHITE_EX+"[+] Fund: "+Fore.LIGHTGREEN_EX,password
+			break
+		else:
+			print Fore.LIGHTWHITE_EX+"[-] Not Fund: "+Fore.LIGHTRED_EX,password
+			sleep(0.5)
+except:
+	print Fore.LIGHTYELLOW_EX+"[!] Please check input "
